8dc66995366d073bb621ae6b72b87dacec540015
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-11-29 19:45:56 -0330

    Initiated the 'Isomorphic Strings' question and included the initial skeleton code

---
+++ Easy/isomorphicStrings.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    
+};
+
+let s = "egg"
+let t = "add"
+console.log(isIsomorphic(s,t))
