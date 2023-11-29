8749a088dec9310081517e79808c0db1f7fed518
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-11-29 13:14:36 -0330

    Implemented the Scilaton code for the Ransom Note problem.

---
+++ Easy/ransomNote.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    
+};
+
+let ransomNote = "a"
+let magazine = "b"
+console.log(canConstruct(ransomNote,magazine))
