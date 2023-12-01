776f4105329e1643bcb9ca55cb1defddcc7b37dd
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-11-30 23:25:54 -0330

    testing isIsomorphic  function

--- Easy/isomorphicStrings.js
+++ Easy/isomorphicStrings.js
@@ -3,10 +3,24 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
-    
+var isIsomorphic = function (s, t) {
+  let newS = "";
+  let hashmap = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (hashmap[s[i]]) {
+      newS += hashmap[s[i]];
+    } else {
+      hashmap[s[i]] = t[i];
+      newS += hashmap[s[i]];
+    }
+  }
+
+  return newS;
 };
 
-let s = "egg"
-let t = "add"
-console.log(isIsomorphic(s,t))
+// let s = "badc";
+// let t = "baba";
+let s = "foo";
+let t = "bar";
+console.log(isIsomorphic(s, t));
