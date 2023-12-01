3bed45cb2a74956697ba129c89b5850310905528
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-11-30 23:47:43 -0330

    Add solution for LeetCode problem "Isomorphic Strings"

--- Easy/isomorphicStrings.js
+++ Easy/isomorphicStrings.js
@@ -1,32 +1,46 @@
 /**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * LeetCode Question: https://leetcode.com/problems/isomorphic-strings/?envType=study-plan-v2&envId=top-interview-150
+ *
+ * Determines if two strings are isomorphic.
+ * @param {string} s - The first string.
+ * @param {string} t - The second string.
+ * @return {boolean} - True if the strings are isomorphic, false otherwise.
  */
-var isIsomorphic = function (s, t) {
-  let newS = "";
-  let sHashmap = {};
-  let tHashmap = {};
+const isIsomorphic = (s, t) => {
+  // Maps to store character mappings between s and t
+  const sMap = new Map();
+  const tMap = new Map();
 
+  // Resulting isomorphic string
+  let newS = "";
+
+  // Iterate through each character in the strings
   for (let i = 0; i < s.length; i++) {
-    if (sHashmap[s[i]]) {
-      newS += sHashmap[s[i]];
+    const sChar = s[i];
+    const tChar = t[i];
+
+    // If the mapping for sChar exists, append it to newS
+    if (sMap.has(sChar)) {
+      newS += sMap.get(sChar);
     } else {
-      if (tHashmap[t[i]]) {
+      // If tChar is already mapped to another sChar, return false
+      if (tMap.has(tChar)) {
         return false;
       }
 
-      sHashmap[s[i]] = t[i];
-      tHashmap[t[i]] = s[i];
-      newS += sHashmap[s[i]];
+      // Create mappings between sChar and tChar
+      sMap.set(sChar, tChar);
+      tMap.set(tChar, sChar);
+
+      // Append tChar to the resulting isomorphic string
+      newS += tChar;
     }
   }
 
+  // Check if the resulting isomorphic string equals t
   return newS === t;
 };
 
-let s = "badc";
-let t = "baba";
-// let s = "foo";
-// let t = "bar";
-console.log(isIsomorphic(s, t));
+// Example usage:
+const result = isIsomorphic("egg", "add");
+console.log(result); // Output: true
