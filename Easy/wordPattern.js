35035bd3a500b01505b3f900060fd255b9790124
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-12-01 20:46:03 -0330

    Initiated the wordPattern problem and included the initial code framework.

---
+++ Easy/wordPattern.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function (pattern, s) {};
+
+let pattern = "abba";
+let s = "dog cat cat dog";
+console.log(wordPattern(pattern, s));
