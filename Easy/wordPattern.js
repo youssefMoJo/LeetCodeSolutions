0bcdc355dcd902d4f14168c156e53056dbb99aa1
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-12-01 22:05:36 -0330

    Add solution for LeetCode problem "Word Pattern"

--- Easy/wordPattern.js
+++ Easy/wordPattern.js
@@ -1,10 +1,39 @@
 /**
+ * Check if a pattern and a string follow the same pattern.
  * @param {string} pattern
  * @param {string} s
  * @return {boolean}
  */
-var wordPattern = function (pattern, s) {};
+var wordPattern = function (pattern, s) {
+  const sArr = s.split(" ");
+  const patternMap = {};
+  const sMap = {};
 
-let pattern = "abba";
-let s = "dog cat cat dog";
+  if (sArr.length !== pattern.length) {
+    return false;
+  }
+
+  for (let i = 0; i < pattern.length; i++) {
+    const currentPatternChar = pattern[i];
+    const currentStringWord = sArr[i];
+
+    if (sMap[currentPatternChar]) {
+      if (sMap[currentPatternChar] !== currentStringWord) {
+        return false;
+      }
+    } else {
+      if (patternMap.hasOwnProperty(currentStringWord)) {
+        return false;
+      }
+      sMap[currentPatternChar] = currentStringWord;
+      patternMap[currentStringWord] = currentPatternChar;
+    }
+  }
+
+  return true;
+};
+
+// Example usage:
+const pattern = "abba";
+const s = "dog constructor constructor dog";
 console.log(wordPattern(pattern, s));
