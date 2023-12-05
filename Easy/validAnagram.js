9a9d2a12205a5142483c4f6b447b5306622fe813
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-12-05 12:37:04 -0330

    Add Valid Anagram Solutions
    "Implementing solutions for valid anagram check:
    
    1. Using sorting approach (isAnagramUsingSorting).
    2. Using character frequency counting (isAnagramUsingFrequency).
    
    Each solution is accompanied by example usage with comments for clarity."

--- Easy/validAnagram.js
+++ Easy/validAnagram.js
@@ -2,13 +2,13 @@
  * LeetCode #242 - Valid Anagram
  * https://leetcode.com/problems/valid-anagram/description/?envType=study-plan-v2&envId=top-interview-150
  *
- * Checks if two strings are anagrams.
+ * Checks if two strings are anagrams using the sorting approach.
  *
  * @param {string} s - The first input string.
  * @param {string} t - The second input string.
  * @return {boolean} - True if the strings are anagrams, false otherwise.
  */
-var isAnagram = function (s, t) {
+var isAnagramUsingSorting = function (s, t) {
   // Convert both strings to arrays of characters,
   // sort them, and then join them back into strings.
   // If the resulting strings are equal, the original
@@ -17,8 +17,50 @@ var isAnagram = function (s, t) {
 };
 
 // Example usage:
-let s = "anagram";
-let t = "nagaram";
+let exampleS = "anagram";
+let exampleT = "nagaram";
 
-// Output the result of the isAnagram function.
-console.log(isAnagram(s, t));
+// Output: true
+console.log(isAnagramUsingSorting(exampleS, exampleT));
+
+/**
+ * Check if two strings are anagrams using character frequency counting.
+ * @param {string} s - The first string.
+ * @param {string} t - The second string.
+ * @return {boolean} True if the strings are anagrams, false otherwise.
+ */
+function isAnagramUsingFrequency(s, t) {
+  // Check if the lengths of the strings are different, if so, they can't be anagrams.
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  // Object to store the frequency of each character in the first string.
+  const charCount = {};
+
+  // Iterate through each character in the first string (s) to populate charCount.
+  for (let char of s) {
+    // Update character count, handle both initialization and incrementing.
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Iterate through each character in the second string (t).
+  for (let char of t) {
+    // If the character is not present in the first string or its count drops below zero, return false.
+    if (!charCount[char]) {
+      return false;
+    }
+    // Decrement the character count to reflect its presence in the second string.
+    charCount[char]--;
+  }
+
+  // If the loops complete without returning false, the strings are anagrams.
+  return true;
+}
+
+// Example usage:
+let newExampleS = "anagram";
+let newExampleT = "nagaram";
+
+// Output: true
+console.log(isAnagramUsingFrequency(newExampleS, newExampleT));
