941ab1cb2f622f15669f8beabb47070f7de552e3
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-12-05 11:35:58 -0330

    started solving "isAnagram" question

---
+++ Easy/validAnagram.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {};
+
+let s = "anagram";
+let t = "nagaram";
+console.log(isAnagram(s, t));
