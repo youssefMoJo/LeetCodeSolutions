f3091cbd7b9d72e065199e12b26f5ff6874ef83d
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-12-06 20:18:27 -0330

    Initiated the solution for the "Contains Duplicate II" question and included the initial skeleton code.

---
+++ Easy/containsDuplicateII.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {};
+
+let nums = [1, 2, 3, 1];
+let k = 3;
+console.log(containsNearbyDuplicate(nums, k));
