0648d2fbcd7b95bc2ccf63c0415c191b95bd6435
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-12-07 17:17:04 -0330

    added some test cases

--- Easy/validParentheses.js
+++ Easy/validParentheses.js
@@ -2,8 +2,11 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {};
+var isValid = function (s) {
+  return s;
+};
 
 let s = "()";
-
+let s1 = "()[]{}";
+let s2 = "(]";
 console.log(isValid(s));
