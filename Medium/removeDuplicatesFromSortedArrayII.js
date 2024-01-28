050a525d10d5da7dc983e723b46d6b7db118f2ec
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-01-27 22:22:19 -0330

    finished solving "Remove Duplicates from Sorted Array II"

--- Medium/removeDuplicatesFromSortedArrayII.js
+++ Medium/removeDuplicatesFromSortedArrayII.js
@@ -2,8 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function (nums) {};
+var removeDuplicates = function (nums) {
+  // Initialize an index variable 'i' to keep track of the valid index for the final result
+  let i = 0;
 
-let nums = [1, 1, 1, 2, 2, 3];
+  // Iterate through each element 'num' in the input array 'nums'
+  for (let num of nums) {
+    // Check if the current index is less than 2 (for the first two elements),
+    // or if the current element is greater than the element two positions back
+    if (i < 2 || num > nums[i - 2]) {
+      // If the condition is met, update the array at index 'i' with the current element
+      nums[i++] = num;
+    }
+  }
+
+  // Return the final length 'i' as the result
+  return i;
+};
+
+let nums = [0, 0, 1, 1, 1, 1, 2, 3, 3];
 
 console.log(removeDuplicates(nums));
