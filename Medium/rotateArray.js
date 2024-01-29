38a3d503c871076496f4724255f878fcfa2fe43f
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-01-29 08:52:16 -0330

    Implemented the skeleton code for the "Rotate Array" problem.

---
+++ Medium/rotateArray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  return true;
+};
+
+let nums = [1, 2, 3, 4, 5, 6, 7];
+let k = 3;
+
+console.log(rotate(nums, k));
