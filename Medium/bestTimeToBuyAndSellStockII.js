241aad4215f05866f4c784fbeddd6234217c9974
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-03-30 16:21:36 -0230

    added the link of the question at the top of the solution

--- Medium/bestTimeToBuyAndSellStockII.js
+++ Medium/bestTimeToBuyAndSellStockII.js
@@ -1,4 +1,5 @@
 /**
+ * Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/?envType=study-plan-v2&envId=top-interview-150
  *
  * @param {number[]} prices
  * @return {number}
