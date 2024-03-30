7be2e47f4d2bb3a927d4480a2f75c90f948fb45a
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-03-30 16:21:18 -0230

    Solved "bestTimeToBuyAndSellStockII" problem

--- Medium/bestTimeToBuyAndSellStockII.js
+++ Medium/bestTimeToBuyAndSellStockII.js
@@ -1,10 +1,28 @@
 /**
+ *
  * @param {number[]} prices
  * @return {number}
  */
 var maxProfit = function (prices) {
-  return prices;
+  let totalProfit = 0;
+  let buy = prices[0]; // Initialize buy price with the first price
+
+  for (let i = 1; i < prices.length; i++) {
+    const currentPrice = prices[i];
+
+    // If the current price is lower than the buy price, update the buy price
+    if (currentPrice < buy) {
+      buy = currentPrice;
+    } else if (currentPrice > buy) {
+      // Calculate the profit and add it to the total profit
+      totalProfit += currentPrice - buy;
+      // Update the buy price to the current price
+      buy = currentPrice;
+    }
+  }
+
+  return totalProfit;
 };
 
-const prices = [7, 1, 5, 3, 6, 4];
+const prices = [1, 2, 3, 4, 5];
 console.log(maxProfit(prices));
