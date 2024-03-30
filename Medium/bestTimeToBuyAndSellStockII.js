17fda1c4e5055f21c849681b71d0e28fe7ca16c3
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-03-30 13:44:48 -0400

    Initiated the question

---
+++ Medium/bestTimeToBuyAndSellStockII.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  return prices;
+};
+
+const prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices));
