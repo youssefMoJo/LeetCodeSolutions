3b9c022000cea688d5ddede2cd758a1896abcfba
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-03-31 16:11:26 -0230

    Solved "jumpGame" problem

--- Medium/jumpGame.js
+++ Medium/jumpGame.js
@@ -3,8 +3,18 @@
  * @return {boolean}
  */
 var canJump = function (nums) {
-  return nums;
+  let maxReach = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxReach) {
+      return false; // If current index cannot be reached
+    }
+    maxReach = Math.max(maxReach, i + nums[i]); // Update maxReach
+    if (maxReach >= nums.length - 1) {
+      return true; // If last index can be reached
+    }
+  }
+  return false; // If last index cannot be reached
 };
 
-const nums = [2, 3, 1, 1, 4];
+const nums = [3, 2, 1, 0, 4];
 console.log(canJump(nums));
