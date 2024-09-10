78064f9f6f5b40b78775bde7e6ba117304f8fdaf
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-09-10 09:10:37 -0230

    Adding skeleton code

---
+++ Easy/mergeTwoSortedLists.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  return list1;
+};
+
+let list1 = [1, 2, 3];
+let list2 = [1, 3, 4];
+
+console.log(mergeTwoLists(list1, list2));
