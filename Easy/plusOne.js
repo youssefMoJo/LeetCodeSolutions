e5629e88935988711014f24447c7ea6607a7f9d3
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-09-12 10:00:33 -0230

    Solved "plusOne" problem

--- Easy/plusOne.js
+++ Easy/plusOne.js
@@ -3,9 +3,18 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i] += 1;
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
   return digits;
 };
 
-const digits = [1, 2, 3];
-
+const digits = [9];
 console.log(plusOne(digits));
