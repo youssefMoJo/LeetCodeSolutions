de17ed08b223665da9bc75d56206e69be4125ada
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-09-14 18:30:20 -0230

    Adding skeleton code

---
+++ Easy/addBinary.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+  return a, b;
+};
+
+let a = "11";
+let b = "1";
+console.log(addBinary(a, b));
