34c65602974529aba563411496efe0b24f7bfd13
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-09-23 13:56:33 -0230

    Adding skeleton code

---
+++ Easy/reverseLinkedList.js
@@ -0,0 +1,30 @@
+// Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+  
+  // Function to convert an array to a linked list
+  function arrayToLinkedList(arr) {
+    let head = new ListNode(arr[0]);
+    let current = head;
+  
+    for (let i = 1; i < arr.length; i++) {
+      current.next = new ListNode(arr[i]);
+      current = current.next;
+    }
+    return head;
+  }
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    return head
+};
+
+let headArray = [1,2,3,4,5]
+let head = arrayToLinkedList(headArray);
+
+console.log(reverseList(head));
