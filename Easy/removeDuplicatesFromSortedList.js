c9dc20674b54b19507e03fec59fea63368991f78
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-09-23 09:23:20 -0230

    Adding skeleton code

---
+++ Easy/removeDuplicatesFromSortedList.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  return head;
+};
+
+let head = [1, 1, 2];
+console.log(deleteDuplicates(head));
