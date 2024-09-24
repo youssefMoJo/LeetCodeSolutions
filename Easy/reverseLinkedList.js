53b774f3f1a7260f57dd17737f7a849cd6f15df6
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-09-24 11:27:57 -0230

    added the link of the question at the top of the solution

--- Easy/reverseLinkedList.js
+++ Easy/reverseLinkedList.js
@@ -1,3 +1,5 @@
+// Problem link: https://leetcode.com/problems/reverse-linked-list/description/?envType=problem-list-v2&envId=linked-list&difficulty=EASY
+
 // Definition for singly-linked list.
 function ListNode(val, next) {
     this.val = val === undefined ? 0 : val;
