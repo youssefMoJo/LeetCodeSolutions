5a142faf2736e9d34af3170f6705ceac81b47d85
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-09-24 11:27:17 -0230

    Solved "reverseLinkedList" problem

--- Easy/reverseLinkedList.js
+++ Easy/reverseLinkedList.js
@@ -21,6 +21,15 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var reverseList = function(head) {
+    let prev = null
+    let curr = head
+    while (curr){
+      let temp = curr.next
+      curr.next = prev
+      prev = curr
+      curr = temp      
+    }
+    head = prev
     return head
 };
 
